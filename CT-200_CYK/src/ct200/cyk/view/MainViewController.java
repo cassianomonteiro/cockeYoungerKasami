1c7b38793b429e77bb031769317e4b5b4e1edc7a
cassianomonteiro <cassianomonteiro@gmail.com>
2014-11-01 01:27:01 -0200

    Adicionado label de resultado à interface.

--- CT-200_CYK/src/ct200/cyk/view/MainViewController.java
+++ CT-200_CYK/src/ct200/cyk/view/MainViewController.java
@@ -2,10 +2,15 @@ package ct200.cyk.view;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 /**
  * Sample Skeleton for "MainView.fxml" Controller Class
@@ -26,15 +31,26 @@ public class MainViewController {
 
     @FXML // fx:id="grammarTextArea"
     private TextArea grammarTextArea; // Value injected by FXMLLoader
+    
+    @FXML // fx:id="resultLabel"
+    private Label resultLabel; // Value injected by FXMLLoader
 
     @FXML // fx:id="testStringTextField"
     private TextField testStringTextField; // Value injected by FXMLLoader
-
 
     // Handler for Button[Button[id=null, styleClass=button]] onMouseClicked
     @FXML
     void evaluateClicked(MouseEvent event) {
         // handle the event here
+    	
+    	Font boldFont = Font.font(
+    			resultLabel.getFont().getFamily(), 
+    			FontWeight.BOLD, 
+    			resultLabel.getFont().getSize());
+    	
+    	resultLabel.setText("Não pertence à linguagem.");
+    	resultLabel.setFont(boldFont);
+    	resultLabel.setTextFill(Color.RED);
     	
     	cykTableTextArea.setText(
     			"It works!\n" +
@@ -49,7 +65,6 @@ public class MainViewController {
         assert testStringTextField != null : "fx:id=\"testStringTextField\" was not injected: check your FXML file 'MainView.fxml'.";
 
         // Initialize your logic here: all @FXML variables will have been injected
-
     }
 
 }
